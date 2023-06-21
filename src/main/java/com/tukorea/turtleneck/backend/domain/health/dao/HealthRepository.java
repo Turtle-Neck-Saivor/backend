7bc340f1bdf9d500ef21786ed92c8a3777e3c19e
wschoe <chldntjr0425@naver.com>
2023-06-22 00:59:41 +0900

    feat: findByMemberEntityAndDate 구현

--- src/main/java/com/tukorea/turtleneck/backend/domain/health/dao/HealthRepository.java
+++ src/main/java/com/tukorea/turtleneck/backend/domain/health/dao/HealthRepository.java
@@ -1,7 +1,20 @@
 package com.tukorea.turtleneck.backend.domain.health.dao;
 
 import com.tukorea.turtleneck.backend.domain.health.domain.HealthInfo;
+import com.tukorea.turtleneck.backend.domain.member.domain.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface HealthRepository extends JpaRepository<HealthInfo, Long> {
+    @Query("SELECT info FROM HealthInfo info WHERE info.memberEntity = :memberEntity " +
+            "AND info.date = :date " +
+            "AND info.isActive = true")
+    List<HealthInfo> findByMemberEntityAndDate(
+            @Param("memberEntity") MemberEntity memberEntity,
+            @Param("date") LocalDate date
+    );
 }
