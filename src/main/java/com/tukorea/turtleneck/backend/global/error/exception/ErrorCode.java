aae4efb62b58539f99155e9b0aefe644fdb6eef7
wschoe <chldntjr0425@naver.com>
2023-06-22 01:00:48 +0900

    feat: Health Info 에러코드 추가

--- src/main/java/com/tukorea/turtleneck/backend/global/error/exception/ErrorCode.java
+++ src/main/java/com/tukorea/turtleneck/backend/global/error/exception/ErrorCode.java
@@ -10,7 +10,8 @@ public enum ErrorCode {
 
     INTERNAL_SERVER_ERROR(500, "G001", "서버 오류"),
     INPUT_INVALID_VALUE(400, "G002", "잘못된 입력"),
-    NOT_FOUND_MEMBER_ENTITY(400, "M001", "존재하지 않는 Member 입니다.")
+    NOT_FOUND_MEMBER_ENTITY(400, "M001", "존재하지 않는 Member 입니다."),
+    NOT_FOUND_HEALTH_INFO_ENTITY(400, "H001", "존재하지 않는 HealthInfo 입니다.")
     ;
 
     private final int status;
