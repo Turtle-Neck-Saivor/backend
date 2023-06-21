8df52f0a8bf1f1ad55bfd4c673b4277d6681b6dd
wschoe <chldntjr0425@naver.com>
2023-06-22 04:00:08 +0900

    fix: 코드 분할

--- src/main/java/com/tukorea/turtleneck/backend/domain/health/service/GraphService.java
+++ src/main/java/com/tukorea/turtleneck/backend/domain/health/service/GraphService.java
@@ -5,6 +5,8 @@ import com.tukorea.turtleneck.backend.domain.health.dao.HealthRepository;
 import com.tukorea.turtleneck.backend.domain.health.domain.HealthInfo;
 import com.tukorea.turtleneck.backend.domain.health.dto.DayGraphInfo;
 import com.tukorea.turtleneck.backend.domain.health.dto.WeekGraphInfo;
+import com.tukorea.turtleneck.backend.domain.health.service.util.GraphTool;
+import com.tukorea.turtleneck.backend.domain.health.service.util.WeekOfDay;
 import com.tukorea.turtleneck.backend.domain.member.dao.MemberRepository;
 import com.tukorea.turtleneck.backend.domain.member.domain.MemberEntity;
 import com.tukorea.turtleneck.backend.domain.member.exception.NotFoundMemberException;
@@ -24,12 +26,13 @@ import java.util.Map;
 public class GraphService {
     private final HealthRepository healthRepository;
     private final MemberRepository memberRepository;
+    private final GraphTool tools;
 
     public DayGraphInfo getDayGraphInfo(LocalDate date, String nickname){
         MemberEntity memberEntity = memberRepository.findMemberEntityByNickname(nickname)
                 .orElseThrow(NotFoundMemberException::new);
         List<HealthInfo> infoList = healthRepository.findByDay(memberEntity, date);
-        long portion = calculatePortion(infoList);
+        long portion = tools.calculatePortion(infoList);
         return DayGraphInfo.builder().portion(portion).build();
     }
 
@@ -48,62 +51,23 @@ public class GraphService {
         }
         for(HealthInfo info : infoList) {
             switch (info.getDate().getDayOfWeek()) {
-                case MONDAY:
-                    addHealthInfo(map, WeekOfDay.MONDAY, info);
-                    break;
-                case TUESDAY:
-                    addHealthInfo(map, WeekOfDay.TUESDAY, info);
-                    break;
-                case WEDNESDAY:
-                    addHealthInfo(map, WeekOfDay.WEDNESDAY, info);
-                    break;
-                case THURSDAY:
-                    addHealthInfo(map, WeekOfDay.THURSDAY, info);
-                    break;
-                case FRIDAY:
-                    addHealthInfo(map, WeekOfDay.FRIDAY, info);
-                    break;
-                case SATURDAY:
-                    addHealthInfo(map, WeekOfDay.SATURDAY, info);
-                    break;
-                case SUNDAY:
-                    addHealthInfo(map, WeekOfDay.SUNDAY, info);
-                    break;
+                case MONDAY: tools.addHealthInfo(map, WeekOfDay.MONDAY, info); break;
+                case TUESDAY: tools.addHealthInfo(map, WeekOfDay.TUESDAY, info); break;
+                case WEDNESDAY: tools.addHealthInfo(map, WeekOfDay.WEDNESDAY, info); break;
+                case THURSDAY: tools.addHealthInfo(map, WeekOfDay.THURSDAY, info); break;
+                case FRIDAY: tools.addHealthInfo(map, WeekOfDay.FRIDAY, info); break;
+                case SATURDAY: tools.addHealthInfo(map, WeekOfDay.SATURDAY, info); break;
+                case SUNDAY: tools.addHealthInfo(map, WeekOfDay.SUNDAY, info); break;
             }
         }
         return WeekGraphInfo.builder()
-                .mon(calculatePortion(map.get(WeekOfDay.MONDAY)))
-                .tue(calculatePortion(map.get(WeekOfDay.TUESDAY)))
-                .wed(calculatePortion(map.get(WeekOfDay.WEDNESDAY)))
-                .thu(calculatePortion(map.get(WeekOfDay.THURSDAY)))
-                .fri(calculatePortion(map.get(WeekOfDay.FRIDAY)))
-                .sat(calculatePortion(map.get(WeekOfDay.SATURDAY)))
-                .sun(calculatePortion(map.get(WeekOfDay.SUNDAY)))
+                .mon(tools.calculatePortion(map.get(WeekOfDay.MONDAY)))
+                .tue(tools.calculatePortion(map.get(WeekOfDay.TUESDAY)))
+                .wed(tools.calculatePortion(map.get(WeekOfDay.WEDNESDAY)))
+                .thu(tools.calculatePortion(map.get(WeekOfDay.THURSDAY)))
+                .fri(tools.calculatePortion(map.get(WeekOfDay.FRIDAY)))
+                .sat(tools.calculatePortion(map.get(WeekOfDay.SATURDAY)))
+                .sun(tools.calculatePortion(map.get(WeekOfDay.SUNDAY)))
                 .build();
-    }
-
-    private long calculatePortion(List<HealthInfo> infoList) {
-        if(infoList.isEmpty()){
-            return 0;
-        }
-        double redCnt = 0.0;
-        double yellowCnt = 0.0;
-        double greenCnt = 0.0;
-        for(HealthInfo info : infoList){
-            redCnt = redCnt + info.getRedCnt();
-            yellowCnt = yellowCnt + info.getYellowCnt();
-            greenCnt = greenCnt + info.getGreenCnt();
-        }
-        double portion =  ((redCnt + yellowCnt) / (redCnt + yellowCnt + greenCnt)) * 100;
-        return (long) portion;
-    }
-
-    private enum WeekOfDay {
-        MONDAY, TUESDAY, THURSDAY, WEDNESDAY, FRIDAY, SATURDAY, SUNDAY
-    }
-
-    private void addHealthInfo(Map<WeekOfDay, List<HealthInfo>> map, WeekOfDay day, HealthInfo healthInfo) {
-        List<HealthInfo> list = map.get(day);
-        list.add(healthInfo);
     }
 }
---
+++ src/main/java/com/tukorea/turtleneck/backend/domain/health/service/util/GraphTool.java
@@ -0,0 +1,29 @@
+package com.tukorea.turtleneck.backend.domain.health.service.util;
+
+import com.tukorea.turtleneck.backend.domain.health.domain.HealthInfo;
+
+import java.util.List;
+import java.util.Map;
+
+public class GraphTool {
+
+    public long calculatePortion(List<HealthInfo> infoList) {
+        if(infoList.isEmpty()){
+            return 0;
+        }
+        double redCnt = 0.0;
+        double yellowCnt = 0.0;
+        double greenCnt = 0.0;
+        for(HealthInfo info : infoList){
+            redCnt = redCnt + info.getRedCnt();
+            yellowCnt = yellowCnt + info.getYellowCnt();
+            greenCnt = greenCnt + info.getGreenCnt();
+        }
+        double portion =  ((redCnt + yellowCnt) / (redCnt + yellowCnt + greenCnt)) * 100;
+        return (long) portion;
+    }
+    public void addHealthInfo(Map<WeekOfDay, List<HealthInfo>> map, WeekOfDay day, HealthInfo healthInfo) {
+        List<HealthInfo> list = map.get(day);
+        list.add(healthInfo);
+    }
+}
