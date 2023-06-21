c3816e91c3ce0576c599221d245b40cb5887e526
wschoe <chldntjr0425@naver.com>
2023-06-22 03:13:05 +0900

    feat: getWeekGraphInfo 서비스 메소드 구현

--- src/main/java/com/tukorea/turtleneck/backend/domain/health/service/GraphService.java
+++ src/main/java/com/tukorea/turtleneck/backend/domain/health/service/GraphService.java
@@ -4,14 +4,20 @@ package com.tukorea.turtleneck.backend.domain.health.service;
 import com.tukorea.turtleneck.backend.domain.health.dao.HealthRepository;
 import com.tukorea.turtleneck.backend.domain.health.domain.HealthInfo;
 import com.tukorea.turtleneck.backend.domain.health.dto.DayGraphInfo;
+import com.tukorea.turtleneck.backend.domain.health.dto.WeekGraphInfo;
 import com.tukorea.turtleneck.backend.domain.member.dao.MemberRepository;
 import com.tukorea.turtleneck.backend.domain.member.domain.MemberEntity;
 import com.tukorea.turtleneck.backend.domain.member.exception.NotFoundMemberException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -19,10 +25,53 @@ public class GraphService {
     private final HealthRepository healthRepository;
     private final MemberRepository memberRepository;
 
-    public DayGraphInfo makeDayGraph(LocalDate date, String nickname){
+    public DayGraphInfo getDayGraphInfo(LocalDate date, String nickname){
         MemberEntity memberEntity = memberRepository.findMemberEntityByNickname(nickname)
                 .orElseThrow(NotFoundMemberException::new);
-        List<HealthInfo> infoList = healthRepository.findByMemberEntityAndDate(memberEntity, date);
+        List<HealthInfo> infoList = healthRepository.findByDay(memberEntity, date);
+        long portion = calculatePortion(infoList);
+        return DayGraphInfo.builder().portion(portion).build();
+    }
+
+    public WeekGraphInfo getWeekGraphInfo(LocalDate date, String nickname) {
+        MemberEntity memberEntity = memberRepository.findMemberEntityByNickname(nickname)
+                .orElseThrow(NotFoundMemberException::new);
+        LocalDate startOfWeek = date.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY));
+        LocalDate endOfWeek = date.with(TemporalAdjusters.nextOrSame(DayOfWeek.SUNDAY));
+        List<HealthInfo> infoList = healthRepository.findByWeek(memberEntity, startOfWeek, endOfWeek);
+        if (infoList == null) {
+            infoList = new ArrayList<>();
+        }
+        Map<WeekOfDay, List<HealthInfo>> map = new EnumMap<>(WeekOfDay.class);
+        for (WeekOfDay day : WeekOfDay.values()) {
+            map.put(day, new ArrayList<>());
+        }
+        for(HealthInfo info : infoList) {
+            switch (info.getDate().getDayOfWeek()) {
+                case MONDAY -> addHealthInfo(map, WeekOfDay.MONDAY, info);
+                case TUESDAY -> addHealthInfo(map, WeekOfDay.TUESDAY, info);
+                case WEDNESDAY -> addHealthInfo(map, WeekOfDay.WEDNESDAY, info);
+                case THURSDAY -> addHealthInfo(map, WeekOfDay.THURSDAY, info);
+                case FRIDAY -> addHealthInfo(map, WeekOfDay.FRIDAY, info);
+                case SATURDAY -> addHealthInfo(map, WeekOfDay.SATURDAY, info);
+                case SUNDAY -> addHealthInfo(map, WeekOfDay.SUNDAY, info);
+            }
+        }
+        return WeekGraphInfo.builder()
+                .mon(calculatePortion(map.get(WeekOfDay.MONDAY)))
+                .tue(calculatePortion(map.get(WeekOfDay.TUESDAY)))
+                .wed(calculatePortion(map.get(WeekOfDay.WEDNESDAY)))
+                .thu(calculatePortion(map.get(WeekOfDay.THURSDAY)))
+                .fri(calculatePortion(map.get(WeekOfDay.FRIDAY)))
+                .sat(calculatePortion(map.get(WeekOfDay.SATURDAY)))
+                .sun(calculatePortion(map.get(WeekOfDay.SUNDAY)))
+                .build();
+    }
+
+    private long calculatePortion(List<HealthInfo> infoList) {
+        if(infoList.isEmpty()){
+            return 0;
+        }
         double redCnt = 0.0;
         double yellowCnt = 0.0;
         double greenCnt = 0.0;
@@ -32,6 +81,15 @@ public class GraphService {
             greenCnt = greenCnt + info.getGreenCnt();
         }
         double portion =  ((redCnt + yellowCnt) / (redCnt + yellowCnt + greenCnt)) * 100;
-        return DayGraphInfo.builder().portion(((long) portion)).build();
+        return (long) portion;
+    }
+
+    private enum WeekOfDay {
+        MONDAY, TUESDAY, THURSDAY, WEDNESDAY, FRIDAY, SATURDAY, SUNDAY
+    }
+
+    private void addHealthInfo(Map<WeekOfDay, List<HealthInfo>> map, WeekOfDay day, HealthInfo healthInfo) {
+        List<HealthInfo> list = map.get(day);
+        list.add(healthInfo);
     }
 }
