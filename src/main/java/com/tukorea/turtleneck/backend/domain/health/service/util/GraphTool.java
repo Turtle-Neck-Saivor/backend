b9d66fac3afb55752b065071269291b7eeb03b75
wschoe <chldntjr0425@naver.com>
2023-06-22 05:31:10 +0900

    fix: Spring Bean으로 추가

--- src/main/java/com/tukorea/turtleneck/backend/domain/health/service/util/GraphTool.java
+++ src/main/java/com/tukorea/turtleneck/backend/domain/health/service/util/GraphTool.java
@@ -1,12 +1,13 @@
 package com.tukorea.turtleneck.backend.domain.health.service.util;
 
 import com.tukorea.turtleneck.backend.domain.health.domain.HealthInfo;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Map;
 
+@Component
 public class GraphTool {
-
     public static long calculatePortion(List<HealthInfo> infoList) {
         if(infoList.isEmpty()){
             return 0;
@@ -25,5 +26,20 @@ public class GraphTool {
     public static void addHealthInfo(Map<WeekOfDay, List<HealthInfo>> map, WeekOfDay day, HealthInfo healthInfo) {
         List<HealthInfo> list = map.get(day);
         list.add(healthInfo);
+    }
+
+    public static long calculateAverage(List<HealthInfo> infoList) {
+        if(infoList.isEmpty()){
+            return 0;
+        }
+        double totalGreen = 0, totalYellow = 0, totalRed = 0;
+        for (HealthInfo info : infoList) {
+            totalGreen += info.getGreenCnt();
+            totalYellow += info.getYellowCnt();
+            totalRed += info.getRedCnt();
+        }
+        double userData = (10 * totalGreen + 27 * totalYellow + 60 * totalRed) /  97.0;
+//        double result = (1.0 / userData) * 100;
+        return (long) userData;
     }
 }
