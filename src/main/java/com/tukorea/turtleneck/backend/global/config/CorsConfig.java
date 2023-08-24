5e049528148dd60849cff77c46e17e611dca7131
wschoe <chldntjr0425@naver.com>
2023-08-24 17:25:28 +0900

    fix: modify cors config to fix 403 code

--- src/main/java/com/tukorea/turtleneck/backend/global/config/CorsConfig.java
+++ src/main/java/com/tukorea/turtleneck/backend/global/config/CorsConfig.java
@@ -1,6 +1,7 @@
 package com.tukorea.turtleneck.backend.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,10 +10,14 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "OPTIONS")
-                .allowedHeaders("headers")
-                .maxAge(3000);
+        registry.addMapping(("/**"))
+                .allowedOrigins("*")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name()
+                );
     }
 }
